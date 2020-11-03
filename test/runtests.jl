ad6cbad9fcd797e6c0c6c4f6fe595dd74520a7b8
Guillaume Lemieux <guilemieux@gmail.com>
2020-11-03 15:23:49 -0500

    add tests and reformat

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,15 +1,32 @@
 using Test
 using Bencode
 
-@test bencode(1) == "i1e"
-@test bencode(123456) == "i123456e"
-@test bencode(0) == "i0e"
-@test bencode(-1234) == "i-1234e"
+@testset "bencode tests" begin
+    @testset "bencode should return a Vector{UInt8}" begin
+        @test typeof(bencode("")) == Vector{UInt8}
+        @test typeof(bencode(0)) == Vector{UInt8}
+        @test typeof(bencode([])) == Vector{UInt8}
+        @test typeof(bencode(Dict())) == Vector{UInt8}
+    end
 
-@test bencode("hello") == "5:hello"
-@test bencode("") == "0:"
+    @testset "bencode with Integer input" begin
+        @test String(bencode(1)) == "i1e"
+        @test String(bencode(123456)) == "i123456e"
+        @test String(bencode(0)) == "i0e"
+        @test String(bencode(-1234)) == "i-1234e"
+    end
 
-@test bencode(["hello", 1, "two"]) == "l5:helloi1e3:twoe"
-@test bencode(1:3) == "li1ei2ei3ee"
-@test bencode([]) == "le"
-@test bencode(Vector{UInt8}("hello")) == ""
+    @testset "bencode with String input" begin
+        @test String(bencode("test")) == "4:test"
+        @test String(bencode("")) == "0:"
+        @test String(bencode("0123456789")) == "10:0123456789"
+        @test String(bencode(":test:")) == "6::test:"
+        @test String(bencode("α😈")) == "6:α😈"  # α is 2 bytes and 😈 is 4 bytes
+    end
+
+    @testset "bencode with List input" begin
+        @test String(bencode(["hello", 1, "two"])) == "l5:helloi1e3:twoe"
+        @test String(bencode(1:3)) == "li1ei2ei3ee"
+        @test String(bencode([])) == "le"
+    end
+end
