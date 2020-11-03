cd882595f68fffdcdc7fa15674bda3db808acdb8
Guillaume Lemieux <guilemieux@gmail.com>
2020-11-03 12:53:40 -0500

    Add bencode for lists

--- src/Bencode.jl
+++ src/Bencode.jl
@@ -4,5 +4,6 @@ export bencode
 
 bencode(s::AbstractString) = string(length(s)) * ":" * s
 bencode(n::Integer) = "i" * string(n) * "e"
+bencode(l::AbstractVector) = "l" * join(map(bencode, l)) * "e"
 
 end # module
