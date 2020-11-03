edf04750452377179f20e61ce57bf6cc9679c8ce
Guillaume Lemieux <guilemieux@gmail.com>
2020-11-03 13:37:25 -0500

    add string and list encoding tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,3 +5,11 @@ using Bencode
 @test bencode(123456) == "i123456e"
 @test bencode(0) == "i0e"
 @test bencode(-1234) == "i-1234e"
+
+@test bencode("hello") == "5:hello"
+@test bencode("") == "0:"
+
+@test bencode(["hello", 1, "two"]) == "l5:helloi1e3:twoe"
+@test bencode(1:3) == "li1ei2ei3ee"
+@test bencode([]) == "le"
+@test bencode(Vector{UInt8}("hello")) == ""
