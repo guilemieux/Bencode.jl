b171d9b33b512fc8184fb68bbf3c9bf0b1e7394a
Guillaume Lemieux <guilemieux@gmail.com>
2020-11-03 15:24:13 -0500

    return byte vectors instead of strings

--- src/Bencode.jl
+++ src/Bencode.jl
@@ -2,8 +2,24 @@ module Bencode
 
 export bencode
 
-bencode(s::AbstractString) = string(length(s)) * ":" * s
-bencode(n::Integer) = "i" * string(n) * "e"
-bencode(l::AbstractVector) = "l" * join(map(bencode, l)) * "e"
+function bencode(s::AbstractString)
+    b = Vector{UInt8}(s)
+    blen = length(b)
+    blenbytestring = Vector{UInt8}(string(blen))
+    vcat(blenbytestring, UInt8(':'), b)
+end
+
+bencode(n::Integer) = Vector{UInt8}("i" * string(n) * "e")
+
+function bencode(l::AbstractVector)
+    if isempty(l)
+        Vector{UInt8}("le")
+    else
+        listcontent = mapreduce(bencode, vcat, l)
+        vcat(UInt8('l'), listcontent, UInt8('e'))
+    end
+end
+
+bencode(d::Dict) = Vector{UInt8}(string(d))
 
 end # module
