87ed42458fe1b6fc94a55ae20b0400105dbac8a3
Guillaume Lemieux <guilemieux@gmail.com>
2020-11-04 10:37:57 -0500

    add test shorthand and add test for bdecode list

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,10 @@
 using Test
 using Bencode
 
+# Shorthand for frequently used methods
+v(s) = Vector{UInt8}(s)
+s(v) = String(copy(v))
+
 @testset "bencode tests" begin
     @testset "bencode should return a Vector{UInt8}" begin
         @test typeof(bencode(Vector{UInt8}([0x00, 0x01]))) == Vector{UInt8}
@@ -11,44 +15,43 @@ using Bencode
     end
 
     @testset "bencode with Vector{UInt8} input" begin
-        @test bencode(Vector{UInt8}([0x00, 0x01])) == 
-                Vector{UInt8}([0x32, 0x3a, 0x00, 0x01])
-        @test bencode(Vector{UInt8}([])) == Vector{UInt8}([0x30, 0x3a])
+        @test bencode(v([0x00, 0x01])) == v([0x32, 0x3a, 0x00, 0x01])
+        @test bencode(v([])) == v([0x30, 0x3a])
     end
 
     @testset "bencode with String input" begin
-        @test String(bencode("test")) == "4:test"
-        @test String(bencode("")) == "0:"
-        @test String(bencode("0123456789")) == "10:0123456789"
-        @test String(bencode(":test:")) == "6::test:"
-        @test String(bencode("α😈")) == "6:α😈"  # α is 2 bytes and 😈 is 4 bytes
+        @test bencode("test") == v("4:test")
+        @test bencode("") == v("0:")
+        @test bencode("0123456789") == v("10:0123456789")
+        @test bencode(":test:") == v("6::test:")
+        @test bencode("α😈") == v("6:α😈")  # α is 2 bytes and 😈 is 4 bytes
     end
 
     @testset "bencode with Integer input" begin
-        @test String(bencode(1)) == "i1e"
-        @test String(bencode(123456)) == "i123456e"
-        @test String(bencode(0)) == "i0e"
-        @test String(bencode(-1234)) == "i-1234e"
+        @test bencode(1) == v("i1e")
+        @test bencode(123456) == v("i123456e")
+        @test bencode(0) == v("i0e")
+        @test bencode(-1234) == v("i-1234e")
     end
 
     @testset "bencode with List input" begin
-        @test String(bencode(["hello", 1, "two"])) == "l5:helloi1e3:twoe"
-        @test String(bencode(1:3)) == "li1ei2ei3ee"
-        @test String(bencode([])) == "le"
+        @test bencode(["hello", 1, "two"]) == v("l5:helloi1e3:twoe")
+        @test bencode(1:3) == v("li1ei2ei3ee")
+        @test bencode([]) == v("le")
     end
 
     @testset "bencode with Dict input" begin
-        @test String(bencode(Dict())) == "de"
-        @test String(bencode(Dict("A" => 1, "B" => "two"))) == "d1:Ai1e1:B3:twoe"
-        @test String(bencode(Dict("B" => "two", "A" => 1))) == "d1:Ai1e1:B3:twoe"
-        @test String(bencode(Dict(
+        @test bencode(Dict()) == v("de")
+        @test bencode(Dict("A" => 1, "B" => "two")) == v("d1:Ai1e1:B3:twoe")
+        @test bencode(Dict("B" => "two", "A" => 1)) == v("d1:Ai1e1:B3:twoe")
+        @test bencode(Dict(
             "string" => "Hello World",
             "integer" => 12345,
             "dict" => Dict(
                 "key" => "value"
             ),
             "list" => [1, 2, "string", 3, Dict()]
-        ))) == "d4:dictd3:key5:valuee7:integeri12345e4:listli1ei2e6:stringi3edee6:string11:Hello Worlde"
+        )) == v("d4:dictd3:key5:valuee7:integeri12345e4:listli1ei2e6:stringi3edee6:string11:Hello Worlde")
     end
 end
 
@@ -61,13 +64,16 @@ end
     end
 
     @testset "bdecode string" begin
-        @test bdecode("4:spam") == Vector{UInt8}("spam")
-        @test bdecode("6:α😈") == Vector{UInt8}("α😈")
-        @test bdecode("10:0123456789") == Vector{UInt8}("0123456789")
-        @test bdecode("6::test:") == Vector{UInt8}(":test:")
+        @test bdecode("4:spam") == v("spam")
+        @test bdecode("6:α😈") == v("α😈")
+        @test bdecode("10:0123456789") == v("0123456789")
+        @test bdecode("6::test:") == v(":test:")
     end
 
     @testset "bdecode list" begin
-        @test bdecode("l5:helloi1e3:twoe") == [Vector{UInt8}("hello"), 1, Vector{UInt8}("two")]
+        @test bdecode("l5:helloi1e3:twoe") == [v("hello"), 1, v("two")]
+        @test bdecode("le") == []
+        @test bdecode("li4e1:2e"; bytestostr=true) == [4, "2"]
+        @test bdecode("l4:spam4:eggse"; bytestostr=true, retnbytesread=true) == (["spam", "eggs"], 14)
     end
 end
